fe204ad3f62f97e27eb81ab781d8af669d4a07c8
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2025-04-30 16:33:13 +0500

    as

--- components/TopHeader.jsx
+++ components/TopHeader.jsx
@@ -4,7 +4,7 @@ import { Mail, Phone, Handshake } from "lucide-react";
 
 export default function TopHeader() {
   return (
-    <div className="bg-green-600 text-white text-sm px-8 py-2 w-full">
+    <div className="bg-green-600 text-white text-sm px-4 py-2 w-full">
       <div className="max-w-8xl mx-auto flex flex-col md:flex-row justify-between items-center space-y-3 md:space-y-0">
         {/* Left Side - Email & Phone */}
         <div className="flex flex-col sm:flex-row items-center sm:space-x-6 space-y-2 sm:space-y-0">
@@ -22,6 +22,7 @@ export default function TopHeader() {
         <a
           href="https://wa.me/+923367000304?text=I want to collaborate in Gaza Relief Activities"
           target="_blank"
+          rel="noopener noreferrer"
           className="bg-white text-green-600 font-semibold px-4 py-1 rounded flex items-center space-x-2 hover:bg-gray-100 transition"
         >
           <Handshake className="w-4 h-4" />
